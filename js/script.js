3961baf1e57241f720e75ad7a2b495983382001e
eduar <eduar.curi73@outlook.com>
2023-02-04 00:22:57 -0500

    terminado

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 let playBtn = document.querySelector(".diferente")
 let audioBtn = document.getElementById("playAudio")
+let mensaje = document.getElementById("mensajeR")
 
 playBtn.addEventListener("click", () => {
     if (audioBtn.paused || audioBtn.ended) {
         playBtn.querySelector(".fa-pause").classList.toggle("oculto")
         playBtn.querySelector(".fa-play").classList.toggle("oculto")
+        mensaje.innerText = "Play"
         audioBtn.play()
     } else {
         playBtn.querySelector(".fa-play").classList.toggle("oculto")
         playBtn.querySelector(".fa-pause").classList.toggle("oculto")
+        mensaje.innerText = "Pause"
         audioBtn.pause()
     }
 })
